d46a2db61b1373ee124f5c6340b0d5cccb5ea77e
Daphne Ng <dng@osisoft.com>
2015-08-11 14:02:51 -0400

    Added 3 projects: helper, console app, WPF app

---
+++ piwebapi_cs_console_test/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using piwebapi_cs_helper;
+using Newtonsoft.Json.Linq;
+
+namespace piwebapi_cs_console_test
+{
+    class Program
+    {
+        /* Console application that makes GET request to a specified URL and display the response
+         * as a string to the console. */
+        static void Main(string[] args)
+        {
+            PIWebAPIClient piWebAPIClient = new PIWebAPIClient();
+            do
+            {
+                try
+                {
+                    Console.Write("Enter URL: ");
+                    string url = Console.ReadLine();
+                    JObject jobj = piWebAPIClient.GetRequest(url);
+                    Console.WriteLine(jobj.ToString());
+                }
+                catch (AggregateException ex)
+                {
+                    foreach (var e in ex.InnerExceptions)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                finally
+                {
+                    Console.WriteLine("Press any key to continue (esc to exit)...");
+                }
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            piWebAPIClient.Dispose();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ piwebapi_cs_wpf_test/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using piwebapi_cs_helper;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+namespace piwebapi_cs_wpf_test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /* This method takes the username and password specified to use basic authentication to connect to 
+         * PI Web API. It then attempts to resolve the tag path provided and write to the tag. */
+        private async void writeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string baseUrl = "https://dng-code.osisoft.int/piwebapi"; //change the baseUrl to your PI Web API service
+            string userName = userNameTextBox.Text;
+            string password = pwBox.Password;
+            string tagPath = tagTextBox.Text;
+            PIWebAPIClient piWebAPIClient = new PIWebAPIClient(userName, password);
+
+            try
+            {
+                //Resolve tag path
+                string requestUrl = baseUrl + "/points/?path=" + tagPath;
+                Task<JObject> tget = piWebAPIClient.GetAsync(requestUrl);
+                statusTextBlock.Text = "Processing...";
+
+                //Attempt to write value to the tag
+                Object payload = new
+                {
+                    value = valueTextBox.Text
+                };
+                string data = JsonConvert.SerializeObject(payload);
+                JObject jobj = await tget;
+                await piWebAPIClient.PostAsync(jobj["Links"]["Value"].ToString(), data);
+
+                //Display final results if successful
+                statusTextBlock.Text = "Write success!";
+            }
+            catch (HttpRequestException ex)
+            {
+                statusTextBlock.Text = ex.Message;
+            }
+            catch (Exception ex)
+            {
+                statusTextBlock.Text = ex.Message;
+            }
+            finally
+            {
+                //We are closing the HttpClient after every write in this simple example. This is not necessary.
+                piWebAPIClient.Dispose();
+            }
+        }
+    }
+}
